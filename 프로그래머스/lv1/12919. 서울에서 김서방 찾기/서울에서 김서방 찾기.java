46425dd8b83cf1877c8711611310aff25513cd73
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-08-18 15:51:45 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 11.11 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -3,6 +3,13 @@ import java.util.*;
 class Solution {
     public String solution(String[] seoul) {    
         // seoul배열을 리스트화 시켜 indexof를 이용해서 출력문을 만든다.
-        return "김서방은 " + List.of(seoul).indexOf("Kim") + "에 있다";
+        // return "김서방은 " + List.of(seoul).indexOf("Kim") + "에 있다";
+        
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                return "김서방은 " + i + "에 있다";
+            }
+        }
+        return "잘못된 값이 입력되는 경우는 없습니다.";
     }
 }
