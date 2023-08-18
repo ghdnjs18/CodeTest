66f7902f78bed90fe08c50ecf585361ec9aebd5b
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-08-18 14:05:29 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 2.56 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
         // 우선순위 큐를 이용해서 큰자리수 자동정렬을 만든다.
         PriorityQueue queue = new PriorityQueue(Collections.reverseOrder());
-        String temp = "";
+        String answer = "";
         
         // 큐에 정수n의 값을 넣어주면 자동정렬이 된다.
         queue.addAll(List.of(String.valueOf(n).split("")));
 
         // 높은순으로 정렬된 큐를 문자열에 순차적으로 넣어준다.
         while (!queue.isEmpty()) {
-            temp += (String) queue.poll();
+            answer += (String) queue.poll();
         }
         
         // 문자열을 정수로 변환하여 값을 입력
-        return answer = Long.parseLong(temp);
+        return Long.parseLong(answer);
     }
 }
