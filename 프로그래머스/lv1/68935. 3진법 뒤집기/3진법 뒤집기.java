8eeac8238b1bc1eb85cf6fa72df89d8413c4e4a7
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-08-20 22:59:58 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        // 10진수의 수를 3진수로 변환하여 StringBuilder에 넣는다.
+        StringBuilder sb = new StringBuilder(Integer.toUnsignedString(n,3));
+        // sb에 문자열을 역순으로 돌려 3진수를 10진수로 변환한다.
+        int answer = Integer.parseInt(sb.reverse().toString(), 3);
+        return answer;
+    }
+}
