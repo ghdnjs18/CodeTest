46f05a6fd9feef4ddbb914394bb4ead7e2b1ab50
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-08-21 21:22:12 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 3.23 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -15,9 +15,7 @@ class Solution {
         // 돈이 -이면 절댓값을 answer에 넣어준다. 아니면 0
         // if (money < 0) {
         //     answer = Math.abs(money);
-        // } else {
-        //     answer = 0;
-        // }
+        // } 
         
         // 스트림을 이용한 문제풀기
         long temp = money - LongStream.rangeClosed(1, count).map(i -> price * i).sum();
