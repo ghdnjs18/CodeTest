57a6eb929809460fd2d9f4bde2ebc195066622e8
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-08-21 11:39:30 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         
         // 3자리수를 뽑아 순차적으로 모든 경우의 수를 계산
-        for (int i = 0; i < number.length; i++) {
-            for (int j = i+1; j < number.length; j++) {
+        for (int i = 0; i < number.length-2; i++) {
+            for (int j = i+1; j < number.length-1; j++) {
                 for (int k = j+1; k < number.length; k++) {
                     if (number[i] + number[j] + number[k] == 0) {
                         answer++;
