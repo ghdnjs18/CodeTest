4c7e8a7d204e741a11dc9895808200da0246eb19
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-09-01 10:09:40 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.18 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[4];
-        int first_x = 0;
-        int last_x = 0;
-        int first_y = 0;
-        int last_y = 0;
+        int first_x = Integer.MAX_VALUE;
+        int last_x = Integer.MIN_VALUE;
+        int first_y = Integer.MAX_VALUE;
+        int last_y = Integer.MIN_VALUE;
 
-
-        boolean first = true;
         for (int i = 0; i < wallpaper.length; i++) {
-            // 처음 나온 폴더의 위치 선택
-            if (first && wallpaper[i].contains("#")) {
-                first = false;
-                first_x = i;
-                first_y = wallpaper[i].indexOf("#");
+            if (wallpaper[i].contains("#")) {
+                first_x = Math.min(first_x, i);
+                last_x = Math.max(last_x, i);
             }
-            // 마지막 줄의 폴더 위치
-            if (wallpaper[i].contains("#")) last_x = Math.max(last_x, i);
             // 가장 앞줄에 있는 폴더 위치
             first_y = Math.min(first_y, wallpaper[i].indexOf("#") < 0 ? first_y : wallpaper[i].indexOf("#"));
             // 가장 뒤에 있는 폴더 위치
