9cfd9862f5932dd099649cdbcff75fde6f99cf8e
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-09-12 10:12:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 8.72 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        // 전체를 소문자로 변경하여 배열로 만듬
+        String[] str = s.toLowerCase().split(" ");
+
+        // 끊어진 문자열을 순환하면서 앞지리를 대문자로 만들고 나머지 문자와 공백을 넣어줌
+        for (String string : str) {
+            if (string.equals("")) answer.append(" ");
+            if (!string.equals("")) answer.append(string.substring(0, 1).toUpperCase() + string.substring(1, string.length()) + " ");
+        }
+
+        // 마지막 문자열에서 공백이 들어가므로 마지막 공백은 삭제
+        if (answer.length() > s.length()) answer.deleteCharAt(answer.lastIndexOf(" "));
+        if (s.length() > answer.length()) answer.append(" ".repeat(s.length() - answer.length()));
+        return answer.toString();
+    }
+}
