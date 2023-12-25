3ae53c01a84da545aecb39d9d8cb48d6e14728cf
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-12-25 15:23:24 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 136 ms, Memory: 14624 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+            int distance = y - x;
+
+            bufferedWriter.write((int)(Math.sqrt(distance) + Math.sqrt(distance-1)) + "\n");
+        }
+        bufferedReader.close();
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
