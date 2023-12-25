94e5476e6954e16f0cb9267f747a982798ada67d
hoho <70429434+ghdnjs18@users.noreply.github.com>
2023-12-25 11:28:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 352 ms, Memory: 26216 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> (Math.abs(o1) == Math.abs(o2)) ? o1 > o2 ? 1 : -1 : Math.abs(o1) > Math.abs(o2) ? 1 : -1);
+
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(bufferedReader.readLine());
+
+            if (number != 0) {
+                queue.offer(number);
+                continue;
+            }
+
+            if (queue.isEmpty()) {
+                bufferedWriter.write(0 + "\n");
+            } else {
+                bufferedWriter.write(queue.poll() + "\n");
+            }
+        }
+        bufferedReader.close();
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+    }
+}
