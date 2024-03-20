6ae4d27cb8bf612e3319d5ec1a837ced6afbf5ee
hoho <70429434+ghdnjs18@users.noreply.github.com>
2024-03-20 12:54:49 +0900

    [Gold V] Title: 내려가기, Time: 180 ms, Memory: 16036 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,18 +4,40 @@ public class Main {
     /*
     * 주어진 값 : N개 줄의 0~9의 숫자 3개씩의 점수
     * 목표 : 한줄당 하나씩 선택해 N번째 줄까지 합의 최대 점수와 최소 점수
-    * 그냥 배열을 이용해서 구하면 실패를 할까? -> 
+    * 그냥 배열을 이용해서 구하면 실패를 할까? -> 성공 : 메모리 27320kb 시간 244ms
     * 배열의 선택 여부에 따라 범위가 계속 변경되면서 선택을 해야한다. -> 투포인트, 슬라이딩 윈도우
     * 범위가 고정되어 있고 예제를 보면 배열이 정렬이 되어 있지 않다. -> 슬라이딩 윈도우
-    * 슬라이딩 윈도우 알고리즘을 이용해서 최대 점수와 최소 점수를 구할 수 있다.
+    * 범위를 입력값과 다음 줄을 값으로 잡고 반복을 하면서 N번째 줄까지 진행하면서 최대 최소 점수를 구한다.
     * */
-
-    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         int N = readNumber();
 
-        arr = new int[N][3];
+        // 슬라이딩 윈도우 알고리즘을 이용한 문제 풀이
+        int[] maxDp = new int[3];
+        int[] minDp = new int[3];
+        for (int i = 0; i < N; i++) {
+            int one = readNumber(), two = readNumber(), thr = readNumber();
+            // max 윈도우
+            int maxOne = Math.max(maxDp[0], maxDp[1]);
+            int maxTwo = Math.max(maxDp[1], maxDp[2]);
+            maxDp[0] = one + maxOne;
+            maxDp[1] = two + Math.max(maxOne, maxTwo);
+            maxDp[2] = thr + maxTwo;
+
+            // min 윈도우
+            int minOne = Math.min(minDp[0], minDp[1]);
+            int minTwo = Math.min(minDp[1], minDp[2]);
+            minDp[0] = one + minOne;
+            minDp[1] = two + Math.min(minOne, minTwo);
+            minDp[2] = thr + minTwo;
+        }
+        int max = Math.max(maxDp[0], Math.max(maxDp[1], maxDp[2]));;
+        int min = Math.min(minDp[0], Math.min(minDp[1], minDp[2]));;
+
+        // 별도 배열을 이용한 문제 풀이
+        /*
+        int[][] arr = new int[N][3];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < 3; j++) {
                 arr[i][j] = readNumber();
@@ -40,6 +62,8 @@ public class Main {
 
         int max = Math.max(maxDp[N-1][0], Math.max(maxDp[N-1][1], maxDp[N-1][2]));;
         int min = Math.min(minDp[N-1][0], Math.min(minDp[N-1][1], minDp[N-1][2]));;
+         */
+
         System.out.println(max + " " + min);
     }
 
