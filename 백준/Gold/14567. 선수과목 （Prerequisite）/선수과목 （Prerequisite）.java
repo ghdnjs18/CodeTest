9c07e44be5d72d42ad9b73b943ff30fc001e2ef9
hoho <70429434+ghdnjs18@users.noreply.github.com>
2024-04-03 11:52:09 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 1096 ms, Memory: 28188 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -2,32 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    /*
+    * 주어진 값 : 과목의 수 N, 조건의 수 M, M개의 연결 관계 A, B
+    * 목표 : 과목 별 처리할 수 있는 최소 기간
+    * 선수 조건의 수가 500000개 완전탐색은 불가능 -> DP, 그리디, 세그트리
+    * A < B의 조건이 있기 때문에 1은 무조건 1 학기 만에 이수할 수 있다.
+    * -> A를 기준으로 연결 관계를 이수할 수 있기 때문에 오름차순 정렬을 한다. -> 연결을 다 확인하기 떄문에 정렬할 필요 없음.
+    * 해당 과목이 B번 과목에 있으면 연결된 A 과목 중 가장 큰값에서 +1을 해준다.
+    * */
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
+        // 과목의 수
+        int N = readNumber();
+        // 연결의 수
+        int M = readNumber();
+
+        // 선수 과목 정보 입력
         int[][] prerequisites = new int[M][2];
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            prerequisites[i][0] = Integer.parseInt(st.nextToken());
-            prerequisites[i][1] = Integer.parseInt(st.nextToken());
+            prerequisites[i][0] = readNumber();
+            prerequisites[i][1] = readNumber();
         }
-        
-        StringBuilder sb = new StringBuilder();
+
+        StringBuilder stringBuilder = new StringBuilder();
 
         int[] dp = new int[N+1];
         for (int i = 1; i <= N; i++) {
             dp[i] = 1;
+
             for (int j = 0; j < M; j++) {
                 if (i == prerequisites[j][1]) {
                     dp[i] = Math.max(dp[i], dp[prerequisites[j][0]] + 1);
                 }
             }
-            sb.append(dp[i]).append(" ");
+            stringBuilder.append(dp[i]).append(" ");
         }
-        
-        System.out.println(sb);
+
+        System.out.println(stringBuilder);
+    }
+
+    private static int readNumber() throws IOException {
+        int cur = System.in.read() & 15;
+        int next = 0;
+        boolean flag = cur == 13;
+        if (flag) cur = 0;
+        while ((next = System.in.read()) > 32) cur = (cur * 10) + (next & 15);
+        return flag ? -cur : cur;
     }
 }
