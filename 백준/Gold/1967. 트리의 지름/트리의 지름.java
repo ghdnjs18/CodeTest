7d52b2ddc5a17189274c52993ca00f5fe3e289b4
hoho <70429434+ghdnjs18@users.noreply.github.com>
2024-04-04 13:09:05 +0900

    [Gold IV] Title: 트리의 지름, Time: 164 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -7,10 +7,17 @@ public class Main {
     * 목표 : 트리의 지름 - 모든 경로중 가장 긴 간선의 합
     * 지름이 가장 길려면 끝에서 끝으로 가야하기 때문에 리프 노드끼리에서만 나올 수 있다.
     * 모든 노드를 탐색하지 않고 리프 노드만 깊이 탐색을 이욯해 가장 긴 지름을 구할 수 있다.
+    * 메모리 119092kb 시간 1592
+    * ----------------
+    * 다른 사람의 풀이를 보고 깨달음
+    * 루트를 기준으로 깊이 탐색을 했을 때, 가장 긴 경로를 가진 리프 노드를 알 수 있다.
+    * -> 루트에서 모든 리프 노드를 탐색이 가능한데 그중에서 가장 길면 트리에서 가장 긴 경로를 가질려면 해당 리프 노드를 지나갈 것이다.
+    * -> 그러면 리프노드를 확인할 필요도 없고 모든 리프 노드가 아닌 해당 리프 노드만 탐색하면 가장 긴 경로를 알 수 있다.
     * */
 
     static List<List<Node>> tree;
     static boolean[] visited;
+    static int leafNode, max;
 
     public static void main(String[] args) throws IOException {
 
@@ -30,26 +37,36 @@ public class Main {
             int nodeB = readNumber();
             int length = readNumber();
 
-            isleaf[nodeA] = true;
+//            isleaf[nodeA] = true;
 
             tree.get(nodeA).add(new Node(nodeB, length));
             tree.get(nodeB).add(new Node(nodeA, length));
         }
 
         // 리프 노드 표시
-        Queue<Integer> leafNode = new LinkedList<>();
-        for (int i = 2; i <= N; i++) {
-            if (!isleaf[i]) {
-                leafNode.offer(i);
-            }
-        }
+//        Queue<Integer> leafNode = new LinkedList<>();
+//        for (int i = 2; i <= N; i++) {
+//            if (!isleaf[i]) {
+//                leafNode.offer(i);
+//            }
+//        }
 
         // 리프 노드 깊이 탐색
-        int max = 0;
-        while (!leafNode.isEmpty()) {
-            visited = new boolean[N + 1];
-            max = Math.max(max, sumNode(leafNode.poll(), 0));
-        }
+//        int max = 0;
+//        while (!leafNode.isEmpty()) {
+//            visited = new boolean[N + 1];
+//            int num = leafNode.poll();
+//            System.out.println(num + " : " + sumNode(num, 0));
+//            max = Math.max(max, sumNode(leafNode.poll(), 0));
+//        }
+
+        visited = new boolean[N + 1];
+        sumNode(1, 0);
+//        System.out.println(leafNode);
+
+        visited = new boolean[N + 1];
+        int max = sumNode(leafNode, 0);
+//        System.out.println(leafNode);
 
         System.out.println(max);
     }
@@ -66,6 +83,12 @@ public class Main {
     private static int sumNode(int cur, int val) {
 
         visited[cur] = true;
+        
+        // 가장 긴 경로와 노드를 확인 가능해 리턴값을 int로 안 잡아도된다.
+        if (val > max) {
+            max = val;
+            leafNode = cur;
+        }
 
         int nextVal = val;
         for (Node next : tree.get(cur)) {
